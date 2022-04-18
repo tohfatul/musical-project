d8f0f8cc97b251bdcbde7ddf2159d89a33f2f7f2
tohfatul <tohfatul@users.noreply.github.com>
2022-04-18 14:21:51 -0500

    css added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express=require('express');
+const path=require('path');
+const res = require('express/lib/response');
 const app=express();
 
-app.use((req, res, next)=>{
-    res.end('page not found');
+// app.use((req, res, next)=>{
+//     res.end('page not found');
+//     next();
+// });
+
+app.use(
+    "/css",
+    express.static(path.join(__dirname, 'public', 'css', 'bootstrap-4.0.0'))
+  );
+
+app.get('/', (req, res, next)=>{
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    
 });
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    });
+
 app.listen(3000, ()=>{
     console.log('Listening on 3000')
 });
